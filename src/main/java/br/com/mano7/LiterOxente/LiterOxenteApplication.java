5598bcd616eb9c215afb297eef7cf2cbcb0ab869
Manoel Bernardo <mbsn2@discente.ifpe.edu.br>
2025-07-16 13:34:13 -0300

    Inicio Projeto

---
+++ src/main/java/br/com/mano7/LiterOxente/LiterOxenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.mano7.LiterOxente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiterOxenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiterOxenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mano7/LiterOxente/LiterOxenteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mano7.LiterOxente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterOxenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
